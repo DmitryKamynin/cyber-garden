27fe1160cf1a6b0f98a00e3f4cfeccdc1949920a
Владимир Бабаев <v_babaev@webjox.ru>
2021-05-28 15:57:27 +0300

    initial backend and telegram bot

---
+++ helphakathon/helphakathon/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for helphakathon project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helphakathon.settings')
+
+application = get_asgi_application()
