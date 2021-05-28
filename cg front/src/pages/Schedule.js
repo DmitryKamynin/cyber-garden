a15ad1955824672fa99c0a3514464dcf98f42444
DmitryKaminin <dak97dak@gmail.com>
2021-05-29 00:46:23 +0300

    Improvement time scale

--- cg front/src/pages/Schedule.js
+++ cg front/src/pages/Schedule.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 
 import {GlobalContext} from '../state/context/globalStateContext';
 
@@ -8,11 +8,9 @@ import Tamplate from '../components/Tamplate'
 
 import styles from '../styles/pages/Schedule.module.css';
 
-import { sortSchedule, getStartEndSchedule } from '../utils';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+import { sortSchedule } from '../utils';
 
 const GetDate = ({date}) => {
-    console.log(date)
     if(date){
         const time = new Date(date);
         const month =   (time.getMonth() + 1) < 10 ? `0${time.getMonth() + 1}` : `${time.getMonth() + 1}`;
@@ -28,38 +26,40 @@ const GetDate = ({date}) => {
 const TimeScale = ({sortedSchedule, currentTime}) => {
 
     const scale = sortedSchedule.slice(currentTime, currentTime + 7);
-    console.log(scale, currentTime);
 
+    const getStyle = (index) => {
+        if( currentTime === index ) return styles.isCurrent;
+        if( currentTime - index === 3) return styles.theSmallest;
+        if( currentTime - index === 2) return styles.isSmall;
+        if( currentTime - index === 1) return styles.isSmaller;
+        else{
+            if( index - currentTime === 3) return styles.theSmallest;
+            if( index - currentTime === 2) return styles.isSmall;
+            if( index - currentTime === 1) return styles.isSmaller;
+            else return styles.empty;
+        }
+    }
 
     return (
         <>
-            <div className={styles.theSmallest}>
-                <GetDate date={scale[0]?.date_time}/> {scale[0]?.title}
-            </div>
+            {sortedSchedule.map(( item, index) => (
+                <div key={item.id} className={getStyle(index)}>
+                    <div style={{display:'flex', alignItems:'center'}}> 
 
-            <div className={styles.isSmall}>
-                <GetDate date={scale[1]?.date_time}/> {scale[1]?.title}
-            </div>
-
-            <div className={styles.isSmaller}>
-                <GetDate date={scale[2]?.date_time}/> {scale[2]?.title}
-            </div>
-
-            <div className={styles.isCurrent}>
-                <GetDate date={scale[3]?.date_time}/> {scale[3]?.title}
-            </div>
-
-            <div className={styles.isSmaller}>
-                <GetDate date={scale[4]?.date_time}/> {scale[4]?.title}
-            </div>
-
-            <div className={styles.isSmall}>
-                <GetDate date={scale[5]?.date_time}/> {scale[5]?.title}
-            </div>
-
-            <div className={styles.theSmallest}>
-                <GetDate date={scale[6]?.date_time}/> {scale[6]?.title}
-            </div>
+                    { currentTime === index ? '[' : ''}
+                    
+                        <div>
+                            <GetDate date={item?.date_time}/>
+                        </div>
+                        
+                        <div style={{marginLeft: '20px'}}> 
+                            {item?.title}
+                        </div>
+                    
+                    { currentTime === index ? ']' : ''}
+                    </div>
+                </div>
+            ))} 
         </>
     );
 };
@@ -75,29 +75,39 @@ export default function Schedule() {
     const sortedSchedule = [...schedule].sort(sortSchedule);
 
     const handleWhell = (e) => {
-        console.log(e)
         if(currentTime === 0 && e.deltaY < 0) return null;
-        if(currentTime === sortedSchedule.length && e.deltaY > 0) return null;
+        if(currentTime === sortedSchedule.length - 1 && e.deltaY > 0) return null;
         else{
             if(e.deltaY > 0) setCurrentTime(currentTime + 1);
             else setCurrentTime(currentTime + -1);
         }
-    }
-    
+    };
+
+    // const handleScroll = (e) => {
+    //     console.log(e)
+    // };
+
+    // useEffect(() => {
+    //     window.addEventListener( 'scroll', handleScroll);
+    //     return () => window.removeEventListener( 'scroll', handleScroll);
+    // },[])
+
+    const timeDiffrent = new Date () -  new Date(sortedSchedule[currentTime]?.date_time);
 
     return (
         <>
             <Tamplate>
-                <div  className={styles.wrapper}>
+                <div onWheel={handleWhell} className={styles.wrapper}>
                     <div className={styles.title}>Расписание событий самого крутого Хакатона</div>
 
                     <div className={styles.timeWrapper}>
                         
                         <div className={styles.target}>
-                            {new Date(sortedSchedule[currentTime + 4]?.date_time) > new Date() ? 'Будет' : "Было"}
+                            {timeDiffrent < 3600000 && timeDiffrent > 0 ? 'Сейчас идёт' : 
+                            new Date(sortedSchedule[currentTime]?.date_time) > new Date() ? 'Будет' : "Было"}
                         </div>
 
-                        <div onWheel={handleWhell} className={styles.timeScale}>
+                        <div className={styles.timeScale}>
                             <TimeScale sortedSchedule={sortedSchedule} currentTime={currentTime}/>
                         </div>
 
